f09df20aaf2ed9ead6dd2d5b88d4735ba48e0144
6Morpheus6 <wanton-decay@gmx.net>
2025-06-23 03:25:32 +0200

    gradio fix

--- install.js
+++ install.js
@@ -28,7 +28,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install gradio==3.50.2 devicetorch",
+          "uv pip install gradio==5.34.2 devicetorch",
           "{{platform === 'darwin' ? 'pip install eva-decord' : 'pip install decord'}}",
           "uv pip install -r ../requirements.txt",
         ]
