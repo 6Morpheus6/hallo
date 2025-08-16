c78065c9698d73536955aa7e56c2701d0241644b
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-08-16 03:05:35 +0200

    torch 2.7.0+cu128 for nvidia

--- torch.js
+++ torch.js
@@ -7,11 +7,8 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": [
-          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128"
-        ]
-      },
-      "next": null
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128"
+      }
     },
     // windows amd
     {
